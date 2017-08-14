f9c900f72fd7037cbe77328937558b8830ae3b0b
saif <saifuddin.ece@gmail.com>
2017-08-14 19:22:05 +0530

    Added script to connect to monogo and save on button click

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var productsRoute = require("./src/routes/productRoutes.js");
+var mongodb = require("mongodb").MongoClient;
+
 
 var app = express();
 
-var productsRoute = require("./src/routes/productRoutes.js");
+var mongoURL = "mongodb://localhost:27017/productsData";
+
 
 
 app.use(express.static('public/template/')); //html file script tags search in this directory for dependencies
@@ -11,29 +15,64 @@ app.use(express.static('public/template/')); //html file script tags search in t
 app.use(express.static("bower_components"));
 
 
-app.set("view engine" , "ejs") //setting view engine, this fixes "Error: No default engine was specified and no extension was provided.", Here key is "view engine"
+app.set("view engine", "ejs") //setting view engine, this fixes "Error: No default engine was specified and no extension was provided.", Here key is "view engine"
 
 app.set("views", "./src/views/")//this fixes - Error: Failed to lookup view "index" in views directory "/Users/vsplash/Documents/mean_workspace/online_store/views"
 
-app.get('/', function(req, response) {
-    
+app.get('/', function (req, response) {
+
+
+    var offersCarouselImages = [
+        { "name": "iwatch", "class": "item active" },
+        { "name": "earpods", "class": "item" },
+        { "name": "mac", "class": "item" },
+        { "name": "phone", "class": "item" },
+        { "name": "spinner", "class": "item" },
+        { "name": "watch", "class": "item" }]
+
     // response.send("Hello");
-    response.render('index', {
-        "nav": [{"name":"Add Product", "link": "products/add"}] 
-        //{"name":"Saif", "link": "saif"}, {"name":"Reshu", "link": "reshu"}
-});
+    //{"name":"Saif", "link": "saif"}, {"name":"Reshu", "link": "reshu"}
+
+
+
+    //----------------------------------------------------------------------------------------
+    mongodb.connect(mongoURL, function (err, db) {
+
+
+        var collection = db.collection("products");
+        
+        collection.find({}).toArray(function (err, results) {
+
+
+            response.render('index', {
+                "nav": [{ "name": "Add Product", "link": "products/add" }],
+                "offers": offersCarouselImages,
+                "mongo_products": mongoURL,
+                "mongo_results" : results
+
+            });
+
+        });
+
+
+    });
+    //----------------------------------------------------------------------------------------
+
+
+
+
 });
 
 var port = "8080";
-app.listen(port, function(err) {
-  
+app.listen(port, function (err) {
+
     if (err) {
-        console.log('error listening @ '+port)
+        console.log('error listening @ ' + port)
     }
     else {
-        console.log('server listening @ '+port)
+        console.log('server listening @ ' + port)
     }
 });
-    
+
 
 app.use("/products", productsRoute);
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,11 +2,14 @@ var express = require("express");
 
 var router = express.Router();
 
+var mongoURL = ""
 
 router.route("/add").get(function(req, res) {
 
     // res.send("Add Product Page");
-    res.render("addProduct", {});
+    res.render("addProduct", {
+        "mongo_products" : mongoURL
+    });
 });
 
 module.exports = router;
